d1576b2bbb6fd2b25e25e08177cb1abfcf72fb19
Heejin Cha <heejincha1219@gmail.com>
2014-09-05 09:46:45 +0900

    Collection.add() Collection.addAll()

--- 01-JavaProgramming/Day05/src/collections/CollectionTest.java
+++ 01-JavaProgramming/Day05/src/collections/CollectionTest.java
@@ -1,9 +1,25 @@
 package collections;
 
+import java.util.ArrayList;
+
 public class CollectionTest {
 	
 	public static void main(String[] args) {
 		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		list.add("Java");
+		list.add("공부가");
+		list.add("재미있나요?");
+		
+		System.out.println(list);
+		
+		ArrayList<String> list2 = new ArrayList<String>();
+		
+		list2.addAll(list);
+		list2.add("정말?");
+		
+		System.out.println(list2);
 		
 		
 	}
