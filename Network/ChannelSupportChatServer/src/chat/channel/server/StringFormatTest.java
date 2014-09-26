4bfc5d8dda39bee69db8d0f10153fbdb29dae37d
Heejin Cha <heejincha1219@gmail.com>
2014-09-26 11:45:19 +0900

    StringFormatTest 수정

--- Network/ChannelSupportChatServer/src/chat/channel/server/StringFormatTest.java
+++ Network/ChannelSupportChatServer/src/chat/channel/server/StringFormatTest.java
@@ -4,11 +4,13 @@ public class StringFormatTest {
 	
 	public static void main(String[] args) {
 		
-		String output = String.format("[%3d] %-40s %-10s", 3, "드루와~", "안영미");
+		int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		
-		System.out.println(output);
-		
-		System.out.println("드루와~                                    ".getBytes().length);
+		for (int i = 0; i < arr.length; i++) {
+			
+			System.out.println(arr[i]);
+			
+		}
 		
 	}
 	
