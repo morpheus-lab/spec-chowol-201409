8014995ce5dba62babdf86098c9e7d7890ac31da
Heejin Cha <heejincha1219@gmail.com>
2014-10-15 15:38:27 +0900

    파일 저장 완료

--- Struts/Day01/src/com/spec/first/board/InsertAction.java
+++ Struts/Day01/src/com/spec/first/board/InsertAction.java
@@ -1,5 +1,9 @@
 package com.spec.first.board;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +30,7 @@ public class InsertAction extends DispatchAction {
 	
 	public ActionForward insert(ActionMapping mapping,
 			ActionForm form,
-			HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+			HttpServletRequest request, HttpServletResponse response) throws IOException {
 		// 클라이언트가 보낸 데이터
 		BoardForm boardForm = (BoardForm) form;
 		
@@ -46,6 +50,7 @@ public class InsertAction extends DispatchAction {
 		for (int i = 0; i < boardForm.getBoardFiles().size(); i++) {
 			FormFile formFile = boardForm.getBoardFiles().get(i);
 			if (formFile != null && formFile.getFileSize() > 0) {
+				// BoardFile DTO 생성
 				BoardFile boardFile = new BoardFile();
 				boardFile.setOriginalName(formFile.getFileName());
 				boardFile.setContentType(formFile.getContentType());
@@ -62,11 +67,71 @@ public class InsertAction extends DispatchAction {
 		return null;
 	}
 	
-	public String saveFile(FormFile formFile) {
+	public String saveFile(FormFile formFile) throws IOException {
 		// 파일시스템에 파일을 저장
 		
+		// 파일 저장 경로
+		String saveDir = "C:/TEMP/";
+		// 파일명에서 확장자 분리
+		String fileName = formFile.getFileName();
+		int lastDotIdx = fileName.lastIndexOf(".");
+		String extName = fileName.substring(lastDotIdx + 1);
+		
+		// 저장할 파일의 이름을 생성
+		String saveFileName = Long.toString(System.currentTimeMillis());
+		
+		// 파일 저장
+		File saveFile = new File(saveDir, saveFileName + "." + extName);
+		
+		int fileNo = 1;
+		
+		while (saveFile.exists()) {
+			// 저장하려는 파일명이 이미 존재한다면, 파일명을 변경
+			saveFile = new File(saveDir,
+					saveFileName + "-" + (fileNo++) + "." + extName);
+		}
+		
+		FileOutputStream fos = new FileOutputStream(saveFile);
+		InputStream in = formFile.getInputStream();
+		
+		byte[] data = new byte[1024];
+		int len = -1;
+		while ((len = in.read(data)) > -1) {
+			fos.write(data, 0, len);
+		}
+		
+		fos.close();
+		in.close();
+		
+		System.out.println("오리지널 파일명: " + formFile.getFileName());
+		System.out.println("저장된 파일명  : " + saveFile.getName());
 		// 저장된 파일의 이름을 리턴
-		return null;
+		return saveFile.getName();
 	}
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
